ec9389c2a901af03c1a52193ec3173d88303d309
morris menanya <emmiemenz@gmail.com>
2025-11-30 11:17:49 +0100

    set env variable

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,4 +1,9 @@
-export const baseUrl = "http://localhost:5000/";
+// export const baseUrl = "http://localhost:5000/";
+
+
+export const baseUrl =
+  import.meta.env.VITE_APP_DEV_BASE_URL ?? "https://flex-living-backend-js.onrender.com/"
+
 export const apiRoute = "api/";
 
 export const publicKey = "MK_PROD_AH8EL53JW8";
